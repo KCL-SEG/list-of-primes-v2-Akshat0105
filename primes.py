c8b453f536a7096ff013288b9d192f6b549d1487
k22002066@kcl.ac.uk <akshat.singh@kcl.ac.uk>
2023-10-05 01:48:04 +0100

    Task completed

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+def isprime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    cont = 0
+    for i in range(1,num+1):
+     if num % i ==0:
+        cont +=1
+    return cont == 2  
+            
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be positive number")
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if isprime(num):
+            list.append(num)
+        num +=1    
     return list
